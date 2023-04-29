833d93b672e72cc8dbe22c958665b4e9c093501e
Rohit-gi <i.rohit003@gmail.com>
2023-04-29 18:40:58 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [todoList, setTodoList] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <div className='addTask'>
+      <input/>
+      <button>Add Task</button>
     </div>
-  );
+    <div className='list'></div>
+    </div>
+  )
 }
 
-export default App;
+export default App
